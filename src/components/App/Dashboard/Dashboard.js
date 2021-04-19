bfe04ef3013fb65845137ecd239e471831ffabdc
dayuss <ddtardian@gmail.com>
2021-04-20 04:57:53 +0700

    merge

--- src/components/App/Dashboard/Dashboard.js
+++ src/components/App/Dashboard/Dashboard.js
@@ -28,140 +28,8 @@ class Dashboard extends Component {
             get_member_baru:[],
             location_data:[],
             location:"-",
-            penjualan_pin:{
-                series: [{
-                    name: 'series1',
-                    data: [31, 40]
-                }, {
-                    name: 'series2',
-                    data: [11, 32]
-                }],
-                options: {
-                    chart: {
-                        height: 350,
-                        type: 'area'
-                    },
-                    dataLabels: {
-                        enabled: false
-                    },
-                    stroke: {
-                        curve: 'smooth'
-                    },
-                    xaxis: {
-                        type: 'date',
-                        categories: ["2018-09-19", "2018-09-19"]
-                    },
-                    tooltip: {
-                        x: {
-                            format: 'dd/MM/yy'
-                        },
-                    },
-                },
-            },
-            penjualan_paket: {
-                series: [{
-                        name: 'series1',
-                        data: [31, 40]
-                    }, {
-                        name: 'series2',
-                        data: [11, 32]
-                    }],
-                    options: {
-                        chart: {
-                            height: 350,
-                            type: 'area'
-                        },
-                        dataLabels: {
-                            enabled: false
-                        },
-                        stroke: {
-                            curve: 'smooth'
-                        },
-                        xaxis: {
-                            type: 'date',
-                            categories: ["2018-09-19", "2018-09-19"]
-                        },
-                        tooltip: {
-                            x: {
-                                format: 'dd/MM/yy'
-                            },
-                        },
-                    },
-            },
-            pie_membership: {
-                series: [44, 55],
-                options: {
-                    labels: ['Team A', 'Team B'],
-                    responsive: [{
-                        breakpoint: 480,
-                        options: {
-                            chart: {
-                                width: 200
-                            },
-                            legend: {
-                                position: 'bottom'
-                            }
-                        }
-                    }]
-                },
-            },
-            pie_karir: {
-                series: [44, 55],
-                options: {
-                    labels: ['Team A', 'Team B'],
-                    responsive: [{
-                        breakpoint: 480,
-                        options: {
-                            chart: {
-                                width: 200
-                            },
-                            legend: {
-                                position: 'bottom'
-                            }
-                        }
-                    }]
-                },
-            },
-            pie_signup: {
-                series: [44, 55],
-                options: {
-                    labels: ['Team A', 'Team B'],
-                    responsive: [{
-                        breakpoint: 480,
-                        options: {
-                            chart: {
-                                width: 200
-                            },
-                            legend: {
-                                position: 'bottom'
-                            }
-                        }
-                    }]
-                },
-            }
-        };
-        this.handleEvent = this.handleEvent.bind(this)
-
-        // socket.on('refresh_dashboard',(data)=>{
-        //     this.refreshData();
-        // })
-        
-        // socket.on("set_dashboard_bo", (data) => {
-        //     this.setState({
-        //         penjualan_pin:data.penjualan_pin,
-        //         penjualan_paket:data.penjualan_paket,
-        //         pie_membership:data.pie_membership,
-        //         pie_karir:data.pie_karir,
-        //         pie_signup:data.pie_signup,
-        //         saldo_member: data.saldo_member,
-        //         total_penarikan: data.total_penarikan,
-        //         total_member: data.total_member,
-        //         total_penjualan: data.total_penjualan,
-        //         get_sponsor_terbaik: data.get_sponsor_terbaik,
-        //         get_member_baru: data.get_member_baru,
-        //     });
-        // });
-    }
+        }
+    };
 
     componentWillReceiveProps = (nextProps) => {
      
@@ -188,16 +56,7 @@ class Dashboard extends Component {
         }
       }
 
-    refreshData(start=null,end=null){
-        // socket.emit('get_dashboard_bo', {
-        //     datefrom: start!==null?start:this.state.startDate,
-        //     dateto: end!==null?end:this.state.endDate,
-        // })
-    }
-
     componentWillMount(){
-        this.refreshData();
-        // this.props.dispatch(CheckDaily());
     }
 
     componentWillUnmount(){
@@ -208,21 +67,7 @@ class Dashboard extends Component {
 
     onChange = date => this.setState({ date })
 
-    handleEvent = (event, picker) => {
-        // end:  2020-07-02T16:59:59.999Z
-        const awal = picker.startDate._d.toISOString().substring(0,10);
-        const akhir = picker.endDate._d.toISOString().substring(0,10);
-        this.setState({
-            startDate:awal,
-            endDate:akhir
-        });
-        this.refreshData(awal,akhir);
-    };
-
-    handleSubmit = (event) => {
-        event.preventDefault()
-        this.refreshData();
-    }
+    
 
     render() {
         return (
@@ -263,111 +108,6 @@ class Dashboard extends Component {
                         {/* Dashboard Widget Area */}
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-md-7 box-margin">
-                        <Chart
-                        data={this.state.penjualan_pin}
-                        title="Penjualan PIN"
-                        type="area"
-                        height={300} />
-                    </div>
-                    <div className="col-md-5 col-xl-5 box-margin">
-                        <Chart
-                        style={{marginTop:'30px'}}
-                        data={this.state.pie_membership}
-                        title="Membership Status"
-                        type="pie"
-                        height={300} />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-5 col-xl-5 box-margin">
-                        <Chart
-                        style={{marginTop:'30px'}}
-                        data={this.state.pie_karir}
-                        title="Jenjang Karir Member"
-                        type="pie"
-                        height={300} />
-                    </div>
-                    <div className="col-md-7 box-margin">
-                        <Chart
-                        data={this.state.penjualan_paket}
-                        title="Penjualan Paket"
-                        type="area"
-                        height={300} />
-                    </div>
-
-                </div>
-                <div className="row">
-                    <div className="col-md-3 col-xl-3 box-margin">
-                        <div className="bgWithOpacity">
-                            <div className="card-header bg-transparent user-area d-flex align-items-center justify-content-between">
-                            <h5 className="card-title mb-0 text-white">Member Baru</h5>
-                            </div>
-                            <div className="card-body" style={{overflowX: 'auto', height: '340px'}}>
-                                <ul className="total-earnings-list">
-                                    {
-                                        this.state.get_member_baru.length > 0 ? this.state.get_member_baru.map((item,i)=>(
-                                            <li key={i}>
-                                                <div className="author-info d-flex align-items-center">
-                                                    <div className="author-img mr-3">
-                                                        <img src={item.picture} onError={(e)=>{e.target.onerror = null; e.target.src=`${Default}`}}  alt={item.full_name}/>
-
-                                                    </div>
-                                                    <div className="author-text">
-                                                    <h6 className="mb-0">{item.full_name}</h6>
-                                                    <p className="mb-0" style={{fontSize:'.7em'}}>Join: {moment(item.created_at).format("yyyy-MM-DD")}</p>
-                                                    </div>
-                                                </div>
-                                                {/*<a href={"#"} className="text-light badge badge-primary">{item.membership}</a>*/}
-                                            </li>
-                                            )
-                                        ):''
-                                    }
-                                </ul>
-                                </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4 col-xl-4 box-margin">
-                        <div className="bgWithOpacity">
-                            <div className="card-header bg-transparent user-area d-flex align-items-center justify-content-between">
-                                <h5 className="card-title mb-0 text-white">10 Sponsor Terbaik</h5>
-                            </div>
-                            <div className="card-body" style={{overflowX: 'auto', height: '340px'}}>
-                                <ul className="total-earnings-list">
-                                    {
-                                        this.state.get_sponsor_terbaik.length > 0 ? this.state.get_sponsor_terbaik.map((item,i)=>(
-                                            <li key={i}>
-                                                <div className="author-info d-flex align-items-center">
-                                                    <div className="author-img mr-3">
-                                                    <img src={item.picture} onError={(e)=>{e.target.onerror = null; e.target.src=`${Default}`}}  alt={item.title}/>
-                                                    </div>
-                                                    <div className="author-text">
-                                                    <h6 className="mb-0">{item.full_name} <img src={item.membership} width={20} alt={item.title} /></h6>
-                                                    <p className="mb-0">{item.jenjang_karir}</p>
-                                                    </div>
-                                                </div>
-                                                <p className="text-light badge badge-warning">{item.sponsor}<br/>Sponsor</p>
-                                            </li>
-                                            )
-                                        ):''
-                                    }
-                                    
-                                </ul>
-                                </div>
-                        </div>
-                    </div>
-                    <div className="col-md-5 col-xl-5 box-margin">
-                        <Chart
-                        style={{marginTop:'30px'}}
-                        data={this.state.pie_signup}
-                        title="Platform Pendaftaran"
-                        type="pie"
-                        height={300} />
-                    </div>
-                </div>
-
-
         </Layout>
        
         );
